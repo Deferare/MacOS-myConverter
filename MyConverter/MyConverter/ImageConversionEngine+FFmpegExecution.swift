16e8b01761f772c5c7dab26aef25cd6579bc7f37
JiHoon K <deferare@icloud.com>
2026-03-06 06:21:42 +0900

    Split image ffmpeg conversion into decode and execution

--- MyConverter/MyConverter/ImageConversionEngine+FFmpegExecution.swift
+++ MyConverter/MyConverter/ImageConversionEngine+FFmpegExecution.swift
@@ -1,35 +1,6 @@
 import Foundation
 
 extension ImageConversionEngine {
-    nonisolated static func ffmpegCanDecodeSource(
-        ffmpegPath: String,
-        inputURL: URL
-    ) -> Bool {
-        let stagedInputURL: URL
-        do {
-            stagedInputURL = try stageInputForFFmpeg(inputURL)
-        } catch {
-            return false
-        }
-        defer {
-            try? OutputPathUtilities.removeFileIfExists(at: stagedInputURL)
-        }
-
-        let result = ProcessCommandRunner.runCommandSync(
-            path: ffmpegPath,
-            arguments: [
-                "-hide_banner",
-                "-loglevel", "error",
-                "-i", stagedInputURL.path,
-                "-map", "0:v:0",
-                "-frames:v", "1",
-                "-f", "null",
-                "-"
-            ]
-        )
-        return result.terminationStatus == 0
-    }
-
     nonisolated static func attemptFFmpegConversion(
         inputURL: URL,
         outputURL: URL,
@@ -142,12 +113,6 @@ extension ImageConversionEngine {
             }
 
             reportProgress(1.0, onProgress: onProgress)
-        }
-    }
-
-    nonisolated private static func stageInputForFFmpeg(_ inputURL: URL) throws -> URL {
-        try FFmpegStagingSupport.stageInputURL(for: inputURL) { code, message in
-            ImageConversionError.ffmpegFailed(code, message)
         }
     }
 
