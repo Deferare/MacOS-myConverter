c35a8b20d48c221a50a7061b275c0c5d8413bbf5
JiHoon K <deferare@icloud.com>
2026-03-14 22:59:38 +0900

    Split capability bootstrap support and behaviors

--- MyConverter/MyConverter/ContentViewModel+CapabilityBootstrap.swift
+++ MyConverter/MyConverter/ContentViewModel+CapabilityBootstrap.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 extension ContentViewModel {
-    struct WarmedDefaultCapability: Sendable {
-        let applyIfIdle: @MainActor @Sendable (ContentViewModel) -> Void
-    }
-
     func markCapabilityBootstrapNeedsRefresh(for kinds: [MediaKind]) {
         capabilityWarmState.markNeedsWarm(for: uniqueMediaKinds(kinds))
     }
@@ -46,44 +42,6 @@ extension ContentViewModel {
             taskState.capabilityBootstrapTask = nil
         }
     }
-
-    func uniqueMediaKinds(_ kinds: [MediaKind]) -> [MediaKind] {
-        var seen: Set<MediaKind> = []
-        return kinds.filter { seen.insert($0).inserted }
-    }
-
-    func applyWarmedDefaultCapabilitiesIfNeeded(_ warmedCapabilities: [WarmedDefaultCapability]) {
-        warmedCapabilities.forEach {
-            $0.applyIfIdle(self)
-        }
-    }
-
-    private func warmDefaultCapabilities(
-        for kinds: [MediaKind]
-    ) async -> [WarmedDefaultCapability] {
-        return await detachedTaskValue(priority: .userInitiated) {
-            await withTaskGroup(
-                of: WarmedDefaultCapability.self,
-                returning: [WarmedDefaultCapability].self
-            ) { group in
-                for kind in kinds {
-                    group.addTask {
-                        kind.warmedDefaultCapability()
-                    }
-                }
-
-                var warmed: [WarmedDefaultCapability] = []
-                for await capability in group {
-                    warmed.append(capability)
-                }
-                return warmed
-            }
-        }
-    }
-
-    private func pendingKindsDescription(for kinds: [MediaKind]) -> String {
-        kinds.map(\.rawValue).joined(separator: ",")
-    }
 }
 
 extension ContentViewModel.MediaKind {
@@ -93,105 +51,5 @@ extension ContentViewModel.MediaKind {
 
     func scheduleCapabilityBootstrap(in viewModel: ContentViewModel) {
         viewModel.scheduleCapabilityBootstrap(for: [self])
-    }
-
-    private struct CapabilityBootstrapBehavior: Sendable {
-        let applyPlaceholderCapabilities: @MainActor @Sendable (ContentViewModel) -> Void
-        let warmedDefaultCapability: @Sendable () -> ContentViewModel.WarmedDefaultCapability
-    }
-
-    nonisolated private static let capabilityBootstrapBehaviorByKind: [Self: CapabilityBootstrapBehavior] = [
-        .video: CapabilityBootstrapBehavior(
-            applyPlaceholderCapabilities: { viewModel in
-                viewModel.applyAvailableOutputFormats(
-                    ContentViewModelSupport.placeholderVideoFormats(),
-                    using: ContentViewModel.videoOutputFormatDescriptor
-                )
-                Self.video.applyPlaceholderCodecOptions(to: viewModel)
-            },
-            warmedDefaultCapability: {
-                let warmedFormats = VideoConversionEngine.defaultOutputFormats()
-                return ContentViewModel.WarmedDefaultCapability { viewModel in
-                    Self.video.applyWarmedOutputFormatsIfIdle(
-                        warmedFormats,
-                        in: viewModel,
-                        formatDescriptor: ContentViewModel.videoOutputFormatDescriptor,
-                        postApply: {
-                            Self.video.refreshCodecOptions(in: viewModel)
-                        }
-                    )
-                }
-            }
-        ),
-        .image: CapabilityBootstrapBehavior(
-            applyPlaceholderCapabilities: { viewModel in
-                viewModel.applyAvailableOutputFormats(
-                    ContentViewModelSupport.placeholderImageFormats(),
-                    using: ContentViewModel.imageOutputFormatDescriptor
-                )
-            },
-            warmedDefaultCapability: {
-                let warmedFormats = ImageConversionEngine.defaultOutputFormats()
-                return ContentViewModel.WarmedDefaultCapability { viewModel in
-                    Self.image.applyWarmedOutputFormatsIfIdle(
-                        warmedFormats,
-                        in: viewModel,
-                        formatDescriptor: ContentViewModel.imageOutputFormatDescriptor
-                    )
-                }
-            }
-        ),
-        .audio: CapabilityBootstrapBehavior(
-            applyPlaceholderCapabilities: { viewModel in
-                viewModel.applyAvailableOutputFormats(
-                    ContentViewModelSupport.placeholderAudioFormats(),
-                    using: ContentViewModel.audioOutputFormatDescriptor
-                )
-                Self.audio.applyPlaceholderCodecOptions(to: viewModel)
-            },
-            warmedDefaultCapability: {
-                let warmedFormats = VideoConversionEngine.defaultAudioOutputFormats()
-                return ContentViewModel.WarmedDefaultCapability { viewModel in
-                    Self.audio.applyWarmedOutputFormatsIfIdle(
-                        warmedFormats,
-                        in: viewModel,
-                        formatDescriptor: ContentViewModel.audioOutputFormatDescriptor,
-                        postApply: {
-                            Self.audio.refreshCodecOptions(in: viewModel)
-                        }
-                    )
-                }
-            }
-        )
-    ]
-
-    nonisolated private var capabilityBootstrapBehavior: CapabilityBootstrapBehavior {
-        Self.capabilityBootstrapBehaviorByKind[self] ?? Self.capabilityBootstrapBehaviorByKind[.video]!
-    }
-
-    func applyPlaceholderCapabilities(to viewModel: ContentViewModel) {
-        capabilityBootstrapBehavior.applyPlaceholderCapabilities(viewModel)
-    }
-
-    func applyWarmedOutputFormatsIfIdle<Format>(
-        _ warmedFormats: [Format],
-        in viewModel: ContentViewModel,
-        formatDescriptor: ContentViewModel.OutputFormatDescriptor<Format>,
-        postApply: () -> Void = {}
-    ) {
-        guard !hasSelectedSource(in: viewModel),
-              !isAnalyzing(in: viewModel) else {
-            return
-        }
-
-        viewModel.applyAvailableOutputFormats(
-            warmedFormats,
-            using: formatDescriptor,
-            postApply: postApply
-        )
-    }
-
-    nonisolated func warmedDefaultCapability() -> ContentViewModel.WarmedDefaultCapability {
-        capabilityBootstrapBehavior.warmedDefaultCapability()
     }
 }
