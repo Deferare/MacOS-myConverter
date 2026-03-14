5e0c26b9261a98b4beb67deb8e5684c9b67daa27
JiHoon K <deferare@icloud.com>
2026-03-14 23:15:50 +0900

    Split selection state and reset helpers

---
+++ MyConverter/MyConverter/ContentViewModel+MediaKind.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension ContentViewModel {
+    enum MediaKind: String, Equatable, Hashable, Sendable, CaseIterable {
+        case video
+        case image
+        case audio
+
+        var sidebarTitle: String {
+            rawValue.capitalized
+        }
+
+        var converterTitle: String {
+            "Convert \(sidebarTitle)"
+        }
+    }
+}
--- MyConverter/MyConverter/ContentViewModel+SelectedFileListState.swift
+++ MyConverter/MyConverter/ContentViewModel+SelectedFileListState.swift
@@ -1,20 +1,6 @@
 import Foundation
 
 extension ContentViewModel {
-    enum MediaKind: String, Equatable, Hashable, Sendable, CaseIterable {
-        case video
-        case image
-        case audio
-
-        var sidebarTitle: String {
-            rawValue.capitalized
-        }
-
-        var converterTitle: String {
-            "Convert \(sidebarTitle)"
-        }
-    }
-
     struct SelectedFileListState: Equatable {
         enum RowStatus: Equatable {
             case pending
@@ -102,84 +88,5 @@ extension ContentViewModel {
     func cancelTask(at keyPath: ReferenceWritableKeyPath<ContentViewModel, Task<Void, Never>?>) {
         self[keyPath: keyPath]?.cancel()
         self[keyPath: keyPath] = nil
-    }
-
-}
-
-extension ContentViewModel.MediaKind {
-    func resetConversionOutputs(in viewModel: ContentViewModel) {
-        setConvertedURL(nil, in: viewModel)
-        setConvertedURLs([], in: viewModel)
-        setConvertedOutputURLsBySourceID([:], in: viewModel)
-        setProcessedSourceIDs([], in: viewModel)
-        setConversionErrorMessage(nil, in: viewModel)
-    }
-
-    func resetCompatibilityState(
-        in viewModel: ContentViewModel,
-        resetMetadata: Bool = true
-    ) {
-        if resetMetadata {
-            resetCompatibilityMetadata(in: viewModel)
-        }
-
-        setCompatibilityMessages(
-            warningMessage: nil,
-            errorMessage: nil,
-            in: viewModel
-        )
-    }
-
-    func resetSelectionCompatibilityState(in viewModel: ContentViewModel) {
-        resetCompatibilityState(in: viewModel)
-    }
-
-    private func clearActivityState(
-        in viewModel: ContentViewModel,
-        resetBatchState: Bool
-    ) {
-        setAnalyzing(false, in: viewModel)
-
-        guard resetBatchState else { return }
-        setCurrentBatchIndex(0, in: viewModel)
-        setTotalBatchCount(0, in: viewModel)
-    }
-
-    func restoreIdleState(
-        in viewModel: ContentViewModel,
-        resetOutputs: Bool = false,
-        resetBatchState: Bool = false,
-        applyDefaultSettings: Bool = false
-    ) {
-        clearPreparedSingleVideoSelection(in: viewModel)
-        cancelSelectionAnalysis(in: viewModel)
-
-        if resetOutputs {
-            resetConversionOutputs(in: viewModel)
-        }
-
-        resetCompatibilityState(in: viewModel)
-        clearActivityState(in: viewModel, resetBatchState: resetBatchState)
-
-        applyPlaceholderCapabilities(to: viewModel)
-
-        if applyDefaultSettings {
-            applyDefaultSourceSettings(to: viewModel)
-        }
-
-        markCapabilityBootstrapNeedsRefresh(in: viewModel)
-        scheduleCapabilityBootstrap(in: viewModel)
-    }
-
-    func clearSelectedSource(in viewModel: ContentViewModel) {
-        clearPreparedSingleVideoSelection(in: viewModel)
-        cancelSelectionAnalysis(in: viewModel)
-        assignSelection([], in: viewModel)
-        restoreIdleState(
-            in: viewModel,
-            resetOutputs: true,
-            resetBatchState: true,
-            applyDefaultSettings: true
-        )
     }
 }
